ea8479b012bd61d3f4e443612711958897d85dd0
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2025-04-15 17:00:47 +0530

    Update main.tf

--- module/node_groups/main.tf
+++ module/node_groups/main.tf
@@ -1,1 +1,40 @@
+resource "aws_iam_role" "node_group_role" {
+  name = var.eks_node_group_name
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+  tags = merge(
+    {
+      Name = format("%s-node_group_iam_role", var.eks_node_group_name)
+    },
+    {
+      "Provisioner" = "Terraform"
+    },
+    var.tags
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node_group_role.name
+}
+
 
